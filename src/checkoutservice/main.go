56e643d2602cc9b8c46b3c1016369d9357cbcc40
Daniel Koss <dkoss@google.com>
2023-12-01 22:02:49 +0000

    manually returning 500 error for checkout service

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -227,6 +227,8 @@ func (cs *checkoutService) Watch(req *healthpb.HealthCheckRequest, ws healthpb.H
 func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest) (*pb.PlaceOrderResponse, error) {
 	log.Infof("[PlaceOrder] user_id=%q user_currency=%q", req.UserId, req.UserCurrency)
 
+        return nil, status.Errorf(codes.Internal, "forced error")
+
 	orderID, err := uuid.NewUUID()
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to generate order uuid")
